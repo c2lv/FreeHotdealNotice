9ca8ce0a655f34aae372a4ee4e6f58212cc1b03f
c2lv <hyeonjun7@naver.com>
2022-10-01 17:50:10 +0900

    Ready to deploy heroku

--- const.py
+++ const.py
@@ -1,5 +1,7 @@
+import os
+
 PROTOCOL_AND_DOMAIN = 'https://www.fmkorea.com'
 PATH = '/index.php'
 PREFIX = '!'
-CH_FREEHOTDEAL_FMKOREA_ID = '' # discord channel id
-TOKEN = '' # discord token
+CH_FREEHOTDEAL_FMKOREA_ID = os.environ.get('CH_FREEHOTDEAL_FMKOREA_ID') # discord channel id
+TOKEN = os.environ.get('BOT_TOKEN') # discord token
