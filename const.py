ea3fb5fe6c99015383ce14abaa94c39e66dc873c
c2lv <hyeonjun7@naver.com>
2022-10-02 03:03:46 +0900

    Modified id type str to int

--- const.py
+++ const.py
@@ -3,5 +3,5 @@ import os
 PROTOCOL_AND_DOMAIN = 'https://www.fmkorea.com'
 PATH = '/index.php'
 PREFIX = '!'
-CH_FREEHOTDEAL_FMKOREA_ID = os.environ.get('CH_FREEHOTDEAL_FMKOREA_ID') # discord channel id
+CH_FREEHOTDEAL_FMKOREA_ID = int(os.environ.get('CH_FREEHOTDEAL_FMKOREA_ID')) # discord channel id
 TOKEN = os.environ.get('BOT_TOKEN') # discord token
